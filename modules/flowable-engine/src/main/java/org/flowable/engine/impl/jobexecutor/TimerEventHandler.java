dcefb5f3e70a5b742b95d10be8a0ee1019de2792
jinyangjie <939249500@qq.com>
2022-02-13 16:51:57 +0800

    定时器事件处理器

--- modules/flowable-engine/src/main/java/org/flowable/engine/impl/jobexecutor/TimerEventHandler.java
+++ modules/flowable-engine/src/main/java/org/flowable/engine/impl/jobexecutor/TimerEventHandler.java
@@ -20,14 +20,24 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * 定时器事件处理器
+ */
 public class TimerEventHandler {
 
+    // 限定固定名称，定时器活动ID的属性名称
     public static final String PROPERTYNAME_TIMER_ACTIVITY_ID = "activityId";
+    // 限定固定名称，截止日期表达式的属性名称
     public static final String PROPERTYNAME_END_DATE_EXPRESSION = "timerEndDate";
+    // 限定固定名称，日程表名称的属性名称
     public static final String PROPERTYNAME_CALENDAR_NAME_EXPRESSION = "calendarName";
 
+    // 创建配置
     public static String createConfiguration(String id, String endDate, String calendarName) {
+        // 实际是从命令上下文中获取到流程引擎配置
         ObjectNode cfgJson = createObjectNode();
+
+        // 重设流程引擎配置中的，定时器活动ID、截止日期表达式、日程表名称
         cfgJson.put(PROPERTYNAME_TIMER_ACTIVITY_ID, id);
         if (endDate != null) {
             cfgJson.put(PROPERTYNAME_END_DATE_EXPRESSION, endDate);
@@ -38,6 +48,7 @@ public class TimerEventHandler {
         return cfgJson.toString();
     }
 
+    // 给配置器设置活动ID
     public static String setActivityIdToConfiguration(String jobHandlerConfiguration, String activityId) {
         try {
             ObjectNode cfgJson = readJsonValueAsObjectNode(jobHandlerConfiguration);
@@ -48,6 +59,7 @@ public class TimerEventHandler {
         }
     }
 
+    // 从配置器获取活动ID
     public static String getActivityIdFromConfiguration(String jobHandlerConfiguration) {
         try {
             JsonNode cfgJson = readJsonValue(jobHandlerConfiguration);
@@ -63,6 +75,7 @@ public class TimerEventHandler {
         }
     }
 
+    // 从配置器获取日程表名称
     public static String getCalendarNameFromConfiguration(String jobHandlerConfiguration) {
         try {
             JsonNode cfgJson = readJsonValue(jobHandlerConfiguration);
@@ -74,17 +87,18 @@ public class TimerEventHandler {
             }
             
         } catch (IOException ex) {
-            // calendar name is not specified
+            // 未指定日程表名称
             return "";
         }
     }
 
+    // 给配置器设置截止时间
     public static String setEndDateToConfiguration(String jobHandlerConfiguration, String endDate) {
         ObjectNode cfgJson = null;
         try {
             cfgJson = readJsonValueAsObjectNode(jobHandlerConfiguration);
         } catch (IOException ex) {
-            // create the json config
+            // 创建json配置
             cfgJson = createObjectNode();
             cfgJson.put(PROPERTYNAME_TIMER_ACTIVITY_ID, jobHandlerConfiguration);
         }
@@ -96,6 +110,7 @@ public class TimerEventHandler {
         return cfgJson.toString();
     }
 
+    // 从配置器获取截止日期
     public static String getEndDateFromConfiguration(String jobHandlerConfiguration) {
         try {
             JsonNode cfgJson = readJsonValue(jobHandlerConfiguration);
@@ -110,16 +125,21 @@ public class TimerEventHandler {
             return null;
         }
     }
-    
+
+    // 创建对象节点
     protected static ObjectNode createObjectNode() {
+        // 从命令上下文工具类中获取流程引擎配置类
         return CommandContextUtil.getProcessEngineConfiguration().getObjectMapper().createObjectNode();
     }
-    
+
+    // 读取JSON值，转换为对象节点
     protected static ObjectNode readJsonValueAsObjectNode(String config) throws IOException {
         return (ObjectNode) readJsonValue(config);
     }
-    
+
+    // 读取JSON值
     protected static JsonNode readJsonValue(String config) throws IOException {
+        // 避免空指针错误
         if (CommandContextUtil.getCommandContext() != null) {
             return CommandContextUtil.getProcessEngineConfiguration().getObjectMapper().readTree(config);
         } else {
